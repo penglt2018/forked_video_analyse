80645a648f1923618840c973ab7fc87857d88d83
penglt <penglt2018@outlook.com>
2019-03-14 22:17:42 +0800

    dd

--- model/right_back/setuptest.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules = cythonize(['/home/mllabs/edward/edward_workspace/video_analyse/model/right_back/pos_recog_model.py']))
